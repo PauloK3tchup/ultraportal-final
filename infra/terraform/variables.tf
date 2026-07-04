ffdc539b7f559129afcb77263657a1ff57c7fe54
PauloK3tchup <passandoalimpo13@gmail.com>
2026-07-04 16:34:07 -0300

    feat(infraestrutura): Estrutura de arquivos do terraform

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+   description = "Regiao AWS"
+   type        = string
+   default     = "us-east-1"
+}
+
+variable "project_name" {
+   description = "Prefixo para nome e tags dos recursos"
+   type        = string
+   default     = "fundamentos-devops"
+}
+
+variable "ami_id" {
+   description = "AMI Ubuntu Server"
+   type        = string
+}
+
+variable "instance_type" {
+   description = "Tipo da instancia EC2"
+   type        = string
+   default     = "t3.micro"
+}
+
+variable "key_name" {
+   description = "Nome da chave SSH cadastrada na AWS"
+   type        = string
+}
+
+variable "allowed_ssh_cidr" {
+   description = "IPs com acesso ao SSH, ex: 203.0.113.10/32 ou 0.0.0.0/0"
+   type        = string
+}
